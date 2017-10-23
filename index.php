ca90067bdbf1ba220998949e55191a7c32da6d50
joeschlueter <JSchlueter12@winona.edu>
2017-10-23 14:56:05 -0500

    search bar

--- index.php
+++ index.php
@@ -3,7 +3,18 @@
 	include "header.php";
 ?>
 	<main>
-		<a href="login/index.php">Log in here</a>
+		<div class="row" style="padding-right: 30px;">
+			<div class="search-bar small-10 medium-10 large-10 small-centered medium-centered large-centered columns">
+				<input type="text" class="searchTerm" name="search" placeholder="Search properties">
+				<button type="submit" class="searchButton"><i class="fa fa-search"></i></button>				
+			</div>
+		</div>
+		<hr>
+		<!--
+			input property info here using database
+		-->
+		<hr>
+			<a href="login/index.php">Log in here</a>
 	</main>
 <?php
 	include "footer.php";
