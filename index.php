0edce39968810733d99da477c32895eb2f565b9f
joeschlueter <JSchlueter12@winona.edu>
2017-10-23 23:09:27 -0500

    small edits

--- index.php
+++ index.php
@@ -6,20 +6,35 @@
 ?>
 	<script src=<?php print($path . "js/search.js") ?> ></script>
 	<main>
-		<div class="row" style="padding-right: 30px;">
+		<div id="searchArea" class="row" style="padding-right: 30px;">
 		<h1 id="title" class="small-10 medium-10 large-10 small-centered medium-centered large-centered" style="margin-top: 20px; margin-bottom: -60px; font-weight: bold;">Rent Smart</h1>
 		<h4 class="small-10 medium-10 large-10 small-centered medium-centered large-centered" style="margin-top: 50px; margin-bottom: -40px; padding-left: 2px;">Renting made smarter.</h4>
 			<div class="search-bar small-10 medium-10 large-10 small-centered medium-centered large-centered columns">
 				<input id="searchBar" type="text" class="searchTerm" name="search" placeholder="Search properties">
 				<button id="searchButton" type="submit" class="searchButton"><i class="fa fa-search"></i></button>				
+				
+			</div>
+			
+		</div>
+		<!--
+		<div class="row">
+			<div class="small-10 medium-10 large-10 small-centered medium-centered large-centered">
+				<label>Filter
+					<select name="price">
+					<option value=""></option>
+						<option value="highest">Highest</option>
+						<option value="lowest">Lowest</option>
+					</select>
+				</label>
 			</div>
 		</div>
+		-->
 		<hr>
 		<!--
 			input property info here using database
 		-->
 		<div class="load">
-		<div class="row small-up-2 medium-up-3 large-up-4">
+		<div class="row small-up-1 medium-up-2 large-up-3">
 		<?php
 			foreach($rows as $row){
 				$address=$row['address'];
@@ -29,8 +44,10 @@
 				$phone=$row['phone'];
 				$landlord=$row['landlord'];
 		?>
-			<div class="column column-block">
-				<p><?=$name?></p>
+			<div id="prop" class="column column-block">
+			<div class="name">
+				<p><a href="#"><?=$name?></a></p>
+			</div>
 				<img src="https://placehold.it/600x400" class="thumbnail" alt="">
 				<div style="padding-left: 10px;">
 					<p>$<?=$price?></p>
--- search/query_search.php
+++ search/query_search.php
@@ -5,7 +5,7 @@
 	}
 	$rows = $db->query("SELECT address, price, name, email, phone, landlord FROM `properties` WHERE address LIKE '%$search%';");
 ?>
-	<div class="row small-up-2 medium-up-3 large-up-4">
+	<div class="row small-up-1 medium-up-2 large-up-3">
 		<?php
 			foreach($rows as $row){
 				$address=$row['address'];
@@ -16,7 +16,9 @@
 				$landlord=$row['landlord'];
 		?>
 			<div class="column column-block">
-				<p><?=$name?></p>
+			<div class="name">
+				<p><a href="#"><?=$name?></a></p>
+			</div>
 				<img src="https://placehold.it/600x400" class="thumbnail" alt="">
 				<div style="padding-left: 10px;">
 					<p>$<?=$price?></p>
