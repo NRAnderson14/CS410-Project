4ea4e71769badabe79e0dadce65176695af3a224
joeschlueter <JSchlueter12@winona.edu>
2017-10-24 17:22:18 -0500

    search works with both keywords in address as well as apartment name

--- search/query_search.php
+++ search/query_search.php
@@ -3,6 +3,7 @@
 	if(isset($_POST['search'])){
 		$search = $_POST['search'];
 	}
+	$rows = $db->query("SELECT address, price, name, email, phone, landlord FROM `properties` WHERE address LIKE '%$search%' OR name LIKE '%$search%';");
 ?>
 	<div class="row small-up-1 medium-up-2 large-up-3">
 		<?php
