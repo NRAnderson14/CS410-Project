ed96024f73427281190d70bccf925dec000daf42
Nathan Anderson <nranderson14@winona.edu>
2017-10-25 13:44:24 -0500

    Quick fix for enter button not working to search

--- js/landlordsearch.js
+++ js/landlordsearch.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
     $('#searchButton').click(function() {
         var term = $('#searchBar').val();
-        console.log("search term is: " + term);
         $('#resarea').load('landlordsearch.php', {'search': term});
     });
-    $('#searchButton').on('keypress', function(e) {
+    $('#searchBar').keypress(function(e) {
         if (e.which === 13) {
-            var term = $("#searchBar").val();
-            $(".load").load("landlordserach.php", {"search": term});
+            var term = $('#searchBar').val();
+            $('#resarea').load('landlordsearch.php', {'search': term});
         }
     })
 });
