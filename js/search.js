d81c8eb33c5392a577825ad0de15bf10b1cde727
joeschlueter <JSchlueter12@winona.edu>
2017-10-25 14:07:42 -0500

    formatting

--- js/search.js
+++ js/search.js
@@ -1,53 +1,40 @@
 $(document).ready(function() {
-	 var city = "";
-navigator.geolocation.getCurrentPosition(success, error);
+	var city = "";
+	navigator.geolocation.getCurrentPosition(success, error);
 
-        function success(position) {
-            console.log(position.coords.latitude)
-            console.log(position.coords.longitude)
+    function success(position) {
+        console.log(position.coords.latitude)
+        console.log(position.coords.longitude)
+        var GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + '%2C' + position.coords.longitude + '&language=en';
 
-            var GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + '%2C' + position.coords.longitude + '&language=en';
-
-            $.getJSON(GEOCODING).done(function(location) {
+        $.getJSON(GEOCODING).done(function(location) {
                 console.log(location)
-            })
-        }
-        function error(err) {
-            console.log(err)
-        }
-		navigator.geolocation.getCurrentPosition(success, error);
-
-        function success(position) {
-
-            var GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + '%2C' + position.coords.longitude + '&language=en';
-
-            $.getJSON(GEOCODING).done(function(location) {
-                
-                city = location.results[0].address_components[2].long_name;
-                //var search = $("#searchBar").val(city)
-				$("#searchBar").val(city);
-		$(".load").load("search/query_search.php", {"search" : city});
-            })
-
-        }
-
-        function error(err) {
-            console.log(err)
-        }
-
-
-		 
-		
+        })
+    }
+	function error(err) {
+		console.log(err)
+    }
 	
-	
+	navigator.geolocation.getCurrentPosition(success, error);
+
+	function success(position) {
+		var GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + '%2C' + position.coords.longitude + '&language=en';
+		$.getJSON(GEOCODING).done(function(location) {
+			city = location.results[0].address_components[2].long_name;
+			$("#searchBar").val(city);
+			$(".load").load("search/query_search.php", {"search" : city});
+		});
+	}
+	function error(err) {
+		console.log(err)
+	}
 	$("#searchButton").click(function(){
-		
-			var search = $("#searchBar").val();
-			if(search == ""){
-				
-			}else{
-				$(".load").load("search/query_search.php", {"search" : search});
-			}
+		var search = $("#searchBar").val();
+		if(search == ""){
+			alert("empty");
+		}else{
+			$(".load").load("search/query_search.php", {"search" : search});
+		}
 	});
 	$("#searchBar").on('keypress', function(e){
 		if(e.which === 13){
@@ -56,9 +43,7 @@ navigator.geolocation.getCurrentPosition(success, error);
 				
 			}else{
 				$(".load").load("search/query_search.php", {"search" : search});
-			}
-			
+			}	
 		}
-		
 	});
 });
