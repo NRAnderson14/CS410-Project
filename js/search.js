1f91a0dad05c847406cbed1fbeceaf8b90ef3f51
joeschlueter <JSchlueter12@winona.edu>
2017-10-25 09:41:07 -0500

    test

--- js/search.js
+++ js/search.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
-	contry_code = google.loader.ClientLocation.address.country_code;
-	city = google.loader.ClientLocation.address.city;
-	region = google.loader.ClientLocation.address.region;
-	$("#searchBar").val(city);
-	$(".load").load("search/query_search.php", {"search" : city});
+	if(google.loader.ClientLocation){
+		contry_code = google.loader.ClientLocation.address.country_code;
+		city = google.loader.ClientLocation.address.city;
+		region = google.loader.ClientLocation.address.region;
+		$("#searchBar").val(city);
+		$(".load").load("search/query_search.php", {"search" : city});
+	}else{
+		city = "Cannot find your location";
+	}
+	
 	$("#searchButton").click(function(){
 		//$.post("query_search.php","val=" + $(this).val(), function(response){
 			var search = $("#searchBar").val();
