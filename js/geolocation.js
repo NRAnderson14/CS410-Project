6083c04a8341137c8039298a3013785098685cd1
joeschlueter <JSchlueter12@winona.edu>
2017-10-26 10:45:47 -0500

    edits

--- js/geolocation.js
+++ js/geolocation.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
 	var city = "";
 	if(google.loader.ClientLocation){
 		city = google.loader.ClientLocation.address.city;
-		$("#searchBar").val(city);
-		$(".load").load("search/query_search.php", {"search" : city});
+		state = google.loader.ClientLocation.address.region;
+		$("#searchBar").val(city + ", " + state);
+		$(".load").load("search/query_search.php", {"search" : city + ", " + state});
 	}else{
 		city = "Couldn't find your location";
 	}
