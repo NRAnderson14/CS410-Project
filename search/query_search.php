36c77906bf19aa98ddbaa5fabcce00d830cb780f
Brenbr <32888803+Brenbr@users.noreply.github.com>
2017-10-26 22:59:00 -0500

    Pagination Attempt
    Doesn't actually work fully, but nothing majorly changed. Can explain for next group meeting. Can be easily deleted.

--- search/query_search.php
+++ search/query_search.php
@@ -1,10 +1,49 @@
+<!-- Style Unsure if it can be anywhere else -->
+<style>
+.pagination{
+  width:  100%;
+  height: 75px;
+  text-align: center;
+  padding-right: 10px;
+}
+
+.pagination a:hover{
+  background-color: var(--main_blue);
+}
+</style>
+
 <?php
     $path = '../';
 	$db = new PDO("mysql:dbname=rent_smart;host=localhost","root");
-	if(isset($_POST['search'])){
-		$search = $_POST['search'];
-	}
-	$rows = $db->query("SELECT property_id, address, price, name, email, phone, landlord, image_url FROM `properties` WHERE address LIKE '%$search%' OR name LIKE '%$search%';");
+  //Original
+  //    if(isset($_POST['search'])){
+  //  	   $search = $_POST['search'];
+  //	}
+  //$rows = $db->query("SELECT SQL_CALC_FOUND_ROWS property_id, address, price, name, email, phone, landlord, image_url FROM `properties` WHERE address LIKE '%$search%' OR name LIKE '%$search%';")
+  //What I changed
+    $propertiesPerPage = 18;
+    $startPage = isset($_GET['startPage']) ? (int)$_GET['page'] : 1;
+    $startPage = ($startPage > 1) ? ($startPage * $propertiesPerPage) - $propertiesPerPage : 0;
+
+  	if(isset($_POST['search'])){
+  	   $search = $_POST['search'];
+  	}
+	$rows = $db->query("SELECT SQL_CALC_FOUND_ROWS
+                                      property_id, address, price, name, email, phone, landlord, image_url
+                                      FROM `properties`
+                                      WHERE address
+                                       LIKE '%$search%'
+                                       OR name
+                                       LIKE '%$search%'
+                                       LIMIT $startPage, $propertiesPerPage;");
+
+  $rowsCount = $db->query("SELECT FOUND_ROWS() as rowsCount") ->fetch()['rowsCount'];
+  $totalPage = (int)($rowsCount / $propertiesPerPage);
+  if ($totalPage > 30){
+    $totalPage = 30;
+  }
+
+  /////////
 ?>
 	<div class="row small-up-1 medium-up-2 large-up-3">
 		<?php
@@ -35,3 +74,12 @@
 			}
 		?>
 		</div>
+
+<!--What I added -->
+    <div class ="pagination">
+      <ul>
+      <?php for($i = 1; $i <= $totalPage; $i++): ?>
+          <li><a href="?startPage=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+      <?php endfor; ?>
+    </ul>
+    </div>
