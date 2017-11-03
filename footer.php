12d286114e227fda0fd3c03511411f5cb25c6e27
joeschlueter <JSchlueter12@winona.edu>
2017-11-03 00:49:59 -0500

    footer

--- footer.php
+++ footer.php
@@ -1,4 +1,25 @@
-		<footer>
+		<footer class="footer">
+			<div class="wrap row small-up-1 medium-up-3 align-center">
+				<div class="column foot">
+				<h4 class="footer-header">Contact Info</h4>
+				<a href=""><p>some text</p></a>
+				<a href=""><p>some text</p></a>
+				<a href=""><p>some text</p></a>
+				</div>
+				<div class="column mid foot">
+				<h4 id="footer-two" class="footer-header">Other Links</h4>
+				<a href=""><p>some text</p></a>
+				<a href=""><p>some text</p></a>
+				<a href=""><p>some text</p></a>
+				</div>
+				<div class="column foot">
+				<h4 class="footer-header">Social Media</h4>
+				<a href=""><p>Facebook <i class="fa fa-facebook" aria-hidden="true"></i></p></a>
+				<a href=""><p>Twitter <i class="fa fa-twitter" aria-hidden="true"></i></p></a>
+				<a href=""><p>Instagram <i class="fa fa-instagram" aria-hidden="true"></i></p></a>
+				</div>
+			</div>
 		</footer>
-	</div>
+</div>
+</div>
 </html>
