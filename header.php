63cf53b730c180d0dd5856ee61dd16d473f0ced9
joeschlueter <JSchlueter12@winona.edu>
2017-11-08 16:18:17 -0600

    Now responsive on mobile devices

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang='en'>
 	<head>
 		<title>Rent Smart</title>
-		<meta charset='utf-8'>
+		<meta charset='utf-8' name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href=<?php print($path . "css/font-awesome/css/font-awesome.min.css") ?>>
 		<link rel="stylesheet" href=<?php print($path . "css/foundation.css") ?> >
 		<link rel="stylesheet" href=<?php print($path . "css/foundation.min.css") ?> >
